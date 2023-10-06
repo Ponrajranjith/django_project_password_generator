4da467402a40344f2bd625a6d342b12cbb0789c4
Ponrajranjith <pranjith100@gmail.com>
2023-10-06 16:32:07 +0530

    create password generator in django

---
+++ number_generator/generator/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+import random
+
+# Create your views here.
+def home(request):
+    return render(request,"generator/index.html")
+
+def password(request):
+    characters = list("1234567890")
+
+    length = int(request.GET.get("length"))
+   
+    password = ""
+    for x in range(length):
+        password = password + random.choice(characters)
+    return render(request,"generator/password.html",{"password":password})
+    
